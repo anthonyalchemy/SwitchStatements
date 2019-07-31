30e82b9f9f98955cfc9ef9851c81760e18efd61c
anthonyalchemy <anthony@alchemytechs.com>
2019-07-31 10:20:03 +0100

    Add project files.

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetDay(80));
+
+            Console.ReadLine();
+
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)//figure out the dayNum then do something
+            {
+                case 0:
+                    dayName = "Sunday"; // if dayNum is 0 = sunday 
+                    break; // break out of structure we are in 
+                case 1:
+                    dayName = "Monday";  
+                    break;
+                case 2:
+                    dayName = "Tuesday"; 
+                    break;
+                case 3:
+                    dayName = "Wednesday";  
+                    break;
+                case 4:
+                    dayName = "Thursday"; 
+                    break;
+                case 5:
+                    dayName = "Friday"; 
+                    break;
+                case 6:
+                    dayName = "Saturday"; 
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+
+
+
+
+            return dayName;
+
+        }
+    }
+}
